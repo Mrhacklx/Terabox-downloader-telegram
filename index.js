4cf4f013cd7ead2766067fe5da0822b7575f59a7
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2024-12-26 17:38:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ async function main() {
   bot.start(async (ctx) => {
     try {
       ctx.reply(
-        `Hi ${ctx.message.from.first_name},\n\nI can Download Files from Terabox.\n\nMade with ❤️ by @botcodes123\n\nSend any terabox link to download.`,
+        `Hi ${ctx.message.from.first_name},\n\nOnline play without any app.\n\nSend any terabox link to Play Online.`,
         Markup.inlineKeyboard([
-          Markup.button.url(" Channel", "https://t.me/botcodes123"),
-          Markup.button.url("Report bug", "https://t.me/Armanidrisi_bot"),
+          Markup.button.url(" Channel", "https://t.me/Tera_online_play")
+          
         ]),
       );
     } catch (e) {
@@ -32,11 +32,17 @@ async function main() {
 
         // ctx.reply(linkID)
 
-        const details = await getDetails(messageText);
-        if (details && details.direct_link) {
+        //const details = await getDetails(messageText);
+        const link1 = await  messageText.replace(/^.*\/s\//, '/s/');
+        const link = await link1.replace('/s/', 'https://terabisgram.blogspot.com/?url=');
+
+        if (link) {
           try {
-            ctx.reply(`Sending Files Please Wait.!!`);
-            sendFile(details.direct_link, ctx);
+            ctx.reply(`| How To Watch Video, Clck here | \n\n| Join this channel for more Updates\n👉 @Tera_online_play |\n\nYour 📽️video link 👇👇`,
+           Markup.inlineKeyboard([
+            Markup.button.url("Online Player🎦", link) ])
+                     );
+            
           } catch (e) {
             console.error(e); // Log the error for debugging
           }
