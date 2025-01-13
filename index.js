73beed4e1092573fa8288b9079124adf1f5c465e
Mrhacklx <142010590+Mrhacklx@users.noreply.github.com>
2025-01-13 11:06:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,74 @@ async function main() {
   const { Telegraf, Markup } = require("telegraf");
   const { getDetails } = require("./api");
   const { sendFile } = require("./utils");
-  const { handleMediaMessage } = require("./MediaMessage");
   const express = require("express");
 
   const bot = new Telegraf(process.env.BOT_TOKEN);
 
-   bot.start(async (ctx) => {
+  // Function to handle media or text messages
+async function handleMediaMessage(ctx, Markup) {
+  let messageText = ctx.message.caption || ctx.message.text || "";
+
+  // Regex to extract URLs
+  const linkRegex = /(https?:\/\/[^\s]+)/g;
+  const links = messageText.match(linkRegex);
+
+  if (links && links.some((link) => link.includes("/s/"))) {
+    const extractedLink = links.find((link) => link.includes("tera") && link.includes("/s/"));
+    const link1 = extractedLink.replace(/^.*\/s\//, "/s/");
+    const link = link1.replace("/s/", "https://terabis.blogspot.com/?url=");
+
+    const responseText1 = `
+🔰 𝙁𝙐𝙇𝙇 𝙑𝙄𝘿𝙀𝙊 🎥👇👇 
+${link}
+
+BACKUP:
+https://t.me/+JZHc9IszlWE1Mzhl 
+
+♡   ❍   ⌲ 
+
+Like   React   Share
+`;
+
+    try {
+      if (ctx.message.photo) {
+        // If it's a photo
+        const photo = ctx.message.photo[ctx.message.photo.length - 1].file_id;
+        await ctx.replyWithPhoto(photo, {
+          caption: responseText1,
+          reply_markup: Markup.inlineKeyboard([
+            Markup.button.url("👉 Online Play🎦", link),
+            Markup.button.url("or Manual Play", "https://terabis.blogspot.com/"),
+          ]),
+        });
+      } else if (ctx.message.video) {
+        // If it's a video
+        const video = ctx.message.video.file_id;
+        await ctx.replyWithVideo(video, {
+          caption: responseText1,
+          reply_markup: Markup.inlineKeyboard([
+            Markup.button.url("👉 Online Play🎦", link),
+            Markup.button.url("or Manual Play", "https://terabis.blogspot.com/"),
+          ]),
+        });
+      } else {
+        // If no media, just reply with the link
+        await ctx.reply(responseText1, Markup.inlineKeyboard([
+          Markup.button.url("👉 Online Play🎦", link),
+          Markup.button.url("or Manual Play", "https://terabis.blogspot.com/"),
+        ]));
+      }
+    } catch (error) {
+      console.error("Error processing media message:", error);
+      ctx.reply("Something went wrong. Please try again later.");
+    }
+  } else {
+    ctx.reply("Please send a valid Terabox link.");
+  }
+}
+
+
+  bot.start(async (ctx) => {
     try {
       ctx.reply(
         `Hi ${ctx.message.from.first_name},\n\nI can Download Files from Terabox.\n\nMade with ❤️ by @botcodes123\n\nSend any terabox link to download.`,
@@ -35,7 +97,7 @@ bot.on("message", async (ctx) => {
   await handleMediaMessage(ctx, Markup);
 });
 
-  
+
   const app = express();
   // Set the bot API endpoint
   app.use(await bot.createWebhook({ domain: process.env.WEBHOOK_URL }));
